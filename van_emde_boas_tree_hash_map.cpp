a5330a346a0151008c20144bbd99c00ab6affadc
Bernardo Amorim <57601237+bernardoamorim@users.noreply.github.com>
2022-10-24 21:25:55 -0300

    Update and rename van_emde_boas_tree.cpp to van_emde_boas_tree_hash_map.cpp

--- van_emde_boas_tree_hash_map.cpp
+++ van_emde_boas_tree_hash_map.cpp
@@ -1,20 +1,9 @@
-#include <iostream>
-#include <cassert>
-#include <set>
-#include <random>
-#include <chrono>
-#include <unordered_map>
-#include <algorithm>
-
+#include <bits/stdc++.h>
+ 
 using namespace std;
-
+ 
 typedef long long ll;
-
-ll builtin_clz(ll x) {
-	for (int i = 0; i < 64; i++) if (x & (1ll << i)) return i;
-	return 64;
-}
-
+ 
 template<int S_SZ>
 class vEBTree {
 public:
@@ -36,7 +25,7 @@ public:
 	bool empty() { 
 		return m_ <= S_SZ ? small_empty() : large_empty(); 
 	}
-
+ 
 private:
 	void in_erase(ll x) {
 		return m_ <= S_SZ ? small_erase(x) : large_erase(x);
@@ -44,14 +33,11 @@ private:
 	void small_recalc() {
 		min_ = M_, max_ = -1;
 		if (tree_ != 0) {
-			// TO DO make better
-			for (int i = 0; i < M_; i++) if (tree_ & (1ll << i)) {
-				if (i < min_) min_ = i;
-				if (i > max_) max_ = i;
-			}
+			min_ = __builtin_ctzll(tree_);
+			max_ = 63 - __builtin_clzll(tree_);
 		}
 	}
-
+ 
 	void small_insert(ll x) {
 		tree_ |= (1ll << x);
 		small_recalc();
@@ -61,15 +47,14 @@ private:
 		small_recalc();
 	}
 	ll small_find_next(ll x) {
-		// TO DO make better
 		if (x == M_ - 1) return M_;
 		ll aux = ((~0ll) << (x + 1)) & tree_;
-		return aux == 0 ? M_ : builtin_clz(aux);
+		return aux == 0 ? M_ : __builtin_ctzll(aux);
 	}
 	bool small_empty() {
 		return tree_ == 0;
 	}
-
+ 
 	pair<ll, ll> partition(ll x) {
 		ll up = x >> (m_ / 2), lo = x - (up << (m_ / 2));
 		return {lo, up};
@@ -81,16 +66,16 @@ private:
 	void fix(ll x) {
 		if (ch_[x]->empty()) ch_.erase(x);
 	}
-
+ 
 	void large_insert(ll x) {
 		if (empty()) min_ = max_ = x;
 		else {
 			if (x < min_) swap(x, min_);
 			max_ = max(max_, x);
-
+ 
 			ll lo, up;
 			tie(lo, up) = partition(x);
-
+ 
 			ch(up)->insert(lo);
 			if (ch(up)->min_ == ch(up)->max_) aux_->insert(up);
 		}
@@ -104,7 +89,7 @@ private:
 			}
 			ll lo, up;
 			tie(lo, up) = partition(x);
-
+ 
 			ch(up)->in_erase(lo);
 			if (ch(up)->empty()) aux_->in_erase(up);
 			fix(up);
@@ -120,10 +105,10 @@ private:
 	ll large_find_next(ll x) {
 		if (x < min_) return min_;
 		if (x >= max_) return M_;
-
+ 
 		ll lo, up;
 		tie(lo, up) = partition(x);
-
+ 
 		if (lo < ch(up)->max_) {
 			ll next = ch(up)->find_next(lo);
 			return next == ch(up)->M_ ? M_ : (up << (m_ / 2)) + next;
@@ -134,58 +119,59 @@ private:
 		}
 	}
 	bool large_empty() { return min_ > max_; }
-
+ 
 	int m_;
 	ll M_, min_, max_, tree_;
 	vEBTree<S_SZ>* aux_;
 	unordered_map<ll, vEBTree<S_SZ>*> ch_;
 };
-
+ 
 int main() {
-
+ 
 	srand(0);
-
+ 
 	int t; cin >> t;
-
-	int m = 24;
-	vEBTree<6> vEB(m);
-
+ 
+	int m = 20;
+	vEBTree<2> vEB(m);
+ 
 	set<int> st;
 	for (int i = 0; i < t; i++) {
 		ll op = rand() % 4, key = rand() % (1ll << m);
-
+ 
 		if (op == 0) {
 			cout << "INSERT " << key << endl;
-			st.insert(key), vEB.insert(key);
+			st.insert(key);
+			vEB.insert(key);
 		}
 		else if (op == 1) {
 			cout << "ERASE " << key << endl;
-			st.erase(key), vEB.erase(key);
+			st.erase(key), 
+			vEB.erase(key);
 		}
 		else if (op == 2) {
 			cout << "FIND NEXT " << key << endl;
-
+ 
 			auto it = st.upper_bound(key);
 			int st_next = (it == st.end() ? (1 << m) : *it);
 			int vEB_next = vEB.find_next(key);
-
+ 
 			cout << "set = " << st_next << endl;
 			cout << "vEB = " << vEB_next << endl;
-
+ 
 			assert(st_next == vEB_next);
 		}
 		else if (op == 3) {
 			cout << "COUNT " << key << endl;
-
-			ll ct_st = st.count(key), ct_vEB = vEB.count(key);
+ 
+			ll ct_st = st.count(key);
+			ll ct_vEB = vEB.count(key);
 			cout << "set = " << ct_st << endl;
 			cout << "vEB = " << ct_vEB << endl;
-
+ 
 			assert(ct_st == ct_vEB);
 		}
-		cout << endl;
-		cout.flush();
 	}
-
+ 
 	return 0;
 }
